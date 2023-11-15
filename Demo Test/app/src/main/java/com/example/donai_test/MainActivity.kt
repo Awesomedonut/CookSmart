d071a77878ea1a070054555fd652c628839847c4
Awesomedonut <Jessicayjsong@gmail.com>
2023-11-14 20:49:13 -0800

    Integrate the demo code

--- Demo Test/app/src/main/java/com/example/donai_test/MainActivity.kt
+++ Demo Test/app/src/main/java/com/example/donai_test/MainActivity.kt
@@ -212,34 +212,5 @@ class MainActivity : ComponentActivity() {
     }
 
     // Function to create an OkHttpClient that trusts all SSL certificates
-    private fun getUnsafeOkHttpClient(): OkHttpClient {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
-                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
-                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-            })
 
-            // Install the all-trusting trust manager
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, trustAllCerts, SecureRandom())
-
-            // Create an ssl socket factory with our all-trusting manager
-            val sslSocketFactory = sslContext.socketFactory
-
-            val builder = OkHttpClient.Builder()
-            // Set custom timeout values here
-            builder.connectTimeout(120, TimeUnit.SECONDS) // Set your desired connection timeout
-            builder.readTimeout(120, TimeUnit.SECONDS)    // Set your desired read timeout
-            builder.writeTimeout(120, TimeUnit.SECONDS)   // Set your desired write timeout
-
-            builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-            builder.hostnameVerifier(HostnameVerifier { _, _ -> true })
-
-            return builder.build()
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
-    }
 }
--- Project/app/build.gradle.kts
+++ Project/app/build.gradle.kts
@@ -7,6 +7,13 @@ android {
     namespace = "com.example.cooksmart"
     compileSdk = 34
 
+//    // New segment to load local.properties and define BuildConfig field
+//    def localPropertiesFile = rootProject.file('local.properties')
+//    def localProperties = new Properties()
+//    if (localPropertiesFile.exists()) {
+//        localProperties.load(new FileInputStream(localPropertiesFile))
+//    }
+//
     defaultConfig {
         applicationId = "com.example.cooksmart"
         minSdk = 24
@@ -15,6 +22,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        buildConfigField "String", "API_URL", "\"${localProperties['API_URL']}\""
     }
 
     buildTypes {
@@ -36,6 +44,9 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+
+
 }
 
 dependencies {
@@ -51,4 +62,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
 }

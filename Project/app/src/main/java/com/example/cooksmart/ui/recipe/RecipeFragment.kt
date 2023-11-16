c5c3e2877cf018a5f6e81f9ccaa4b9c9cc6b5c1e
Haruka Mibuchi <harukamibuchi@d142-058-043-047.wireless.sfu.ca>
2023-11-15 22:41:54 -0800

    Eddited Recipe fragment and layout

--- Project/app/src/main/java/com/example/cooksmart/ui/recipe/RecipeFragment.kt
+++ Project/app/src/main/java/com/example/cooksmart/ui/recipe/RecipeFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cooksmart.R
 import com.example.cooksmart.databinding.FragmentRecipeBinding
+import com.example.cooksmart.ui.fridge.ListAdapter
 
 class RecipeFragment : Fragment() {
 
@@ -22,6 +27,11 @@ class RecipeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val layout = inflater.inflate(R.layout.fragment_recipe,container,false)
+        val adapter = ListAdapter()
+        val listView = layout.findViewById<ListView>(R.id.recipe_list)
+        listView.adapter = adapter
+        listView.layoutManager = LinearLayoutManager(requireContext())
         val homeViewModel =
             ViewModelProvider(this).get(RecipeViewModel::class.java)
 

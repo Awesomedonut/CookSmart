e95c6dca8fcba35051ff686864b0b3c115e4e096
Sarah <scli@sfu.ca>
2023-11-15 23:16:00 -0800

    Revert add ingredient xml, add Parcelable to dependencies

--- Project/app/src/main/java/com/example/cooksmart/database/Ingredient.kt
+++ Project/app/src/main/java/com/example/cooksmart/database/Ingredient.kt
@@ -1,8 +1,12 @@
 package com.example.cooksmart.database
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity(tableName = "ingredient_table")
 data class Ingredient (
     @PrimaryKey(autoGenerate = true)
@@ -12,4 +16,4 @@ data class Ingredient (
     var quantity: String,
     var dateAdded: Long,
     var bestBefore: Long
-)
+): Parcelable

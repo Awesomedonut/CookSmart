af02d1697a611eb98c7aa598a3a9bff6c6094b6c
Amanda <88062177+amandahn@users.noreply.github.com>
2023-11-19 20:46:15 -0800

    - Updated gradle file to match main

--- Project/app/build.gradle.kts
+++ Project/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_URL", localProperties["API_URL"] as String)
-//        buildConfigField("String", "AUDIO_URL", localProperties["AUDIO_URL"] as String)
-//        buildConfigField("String", "AUDIO_FILE_WEB_DOMAIN", localProperties["AUDIO_FILE_WEB_DOMAIN"] as String)
+        buildConfigField("String", "AUDIO_URL", localProperties["AUDIO_URL"] as String)
+        buildConfigField("String", "AUDIO_FILE_WEB_DOMAIN", localProperties["AUDIO_FILE_WEB_DOMAIN"] as String)
         buildConfigField("String", "OPEN_AI_API", localProperties["OPEN_AI_API"] as String)
     }
 

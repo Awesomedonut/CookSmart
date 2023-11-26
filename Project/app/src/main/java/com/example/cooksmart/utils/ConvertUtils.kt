a71e06b718f52522bfc0f80b1ec4abe8e3da16e1
Sarah <scli@sfu.ca>
2023-11-26 01:51:03 -0800

    Fix bug with parsing string to list of strings

--- Project/app/src/main/java/com/example/cooksmart/utils/ConvertUtils.kt
+++ Project/app/src/main/java/com/example/cooksmart/utils/ConvertUtils.kt
@@ -9,8 +9,9 @@ class ConvertUtils {
         // From https://stackoverflow.com/questions/53512575/how-to-convert-a-string-sentence-to-arraylist-in-kotlin
         fun stringToArrayList(str: String): List<String> {
             val strNoArray = str.substring(1, str.length-1)
-            val list = strNoArray.trim().splitToSequence(',', ' ').filter{it.isNotEmpty()}.toList()
-            return ArrayList<String>(list)
+            val list = strNoArray.trim().splitToSequence(',').filter{it.isNotEmpty()}.toList()
+            val trimmedSpacesList = list.map {it.trim()}
+            return ArrayList<String>(trimmedSpacesList)
         }
 
         // SimpleDateFormat from https://developer.android.com/reference/kotlin/android/icu/text/SimpleDateFormat

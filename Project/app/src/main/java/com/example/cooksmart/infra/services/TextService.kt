27ea18668d66eae7732f486ea5d69189293b45f0
Awesomedonut <Jessicayjsong@gmail.com>
2023-11-28 12:16:25 -0800

    wrap the stream call with try/catch

--- Project/app/src/main/java/com/example/cooksmart/infra/services/TextService.kt
+++ Project/app/src/main/java/com/example/cooksmart/infra/services/TextService.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import kotlin.reflect.KSuspendFunction1
 
 class TextService(private val openAI: OpenAI) {
 
@@ -35,8 +36,10 @@ class TextService(private val openAI: OpenAI) {
         onTextUpdated:((text: String, promptId: Int) -> Unit),  // Made nullable
         onAudioTextReady: ((text: String, promptId: Int) -> Unit)? = null,  // Made nullable
         onSummaryReady: ((text: String, promptId: Int) -> Unit)? = null,    // Made nullable
-        onCompleted: ((promptId: Int) -> Unit)? = null
-    ) {
+        onCompleted: ((promptId: Int) -> Unit)? = null,
+        onCompletedSuspend: (KSuspendFunction1<Int, Unit>)? = null,
+        onError: ((text: String) -> Unit)? = null,
+        ) {
         coroutineScope.launch(Dispatchers.IO) {
             println("\n>️ Streaming chat completions...: ${promptBag.text}")
             val chatCompletionRequest = ChatCompletionRequest(
@@ -51,42 +54,57 @@ class TextService(private val openAI: OpenAI) {
                     )
                 )
             )
-            openAI.chatCompletions(chatCompletionRequest)
-                .onStart {
-                    resetText()
-                }
-                .onEach { response ->
-                    val text = response.choices.firstOrNull()?.delta?.content.orEmpty()
-                    fullText += text
-                    val firstNewLineIndex = fullText.indexOf("\n\n", tempIndex)
-                    if (firstNewLineIndex > 0) {
-                        audioText = fullText.substring(startIndex, firstNewLineIndex)
-                        if(audioCount == 0 || audioText.length > 50) {
-                            onAudioTextReady?.invoke(audioText,promptBag.promptId)  // Call only if not null
-                            startIndex = firstNewLineIndex + 1
-                            tempIndex = startIndex
-                            audioCount ++
-                            if(audioCount > 1 && !summarySent){
-                                onSummaryReady?.invoke(fullText,promptBag.promptId)  // Call only if not null
-                                summarySent = true
-                            }
-                        }else{
-                            //force to move to the next paragraph
-                            tempIndex = firstNewLineIndex + 1
-                        }
-                        Log.d("TextService", "Sending one paragraph of audio")
+            try {
+                openAI.chatCompletions(chatCompletionRequest)
+                    .onStart {
+                        resetText()
                     }
-                    //responseState.postValue(fullText)
-                    onTextUpdated(fullText,promptBag.promptId)
-                }
-                .onCompletion {
-                    onAudioTextReady?.invoke(fullText.substring(startIndex),promptBag.promptId)  // Call only if not null
-                    //responseState.postValue(fullText)
-                    onTextUpdated(fullText,promptBag.promptId)
-                    resetText()
-                    onCompleted?.invoke(promptBag.promptId)
-                }
-                .launchIn(coroutineScope)
+                    .onEach { response ->
+                        val text = response.choices.firstOrNull()?.delta?.content.orEmpty()
+                        fullText += text
+                        val firstNewLineIndex = fullText.indexOf("\n\n", tempIndex)
+                        if (firstNewLineIndex > 0) {
+                            audioText = fullText.substring(startIndex, firstNewLineIndex)
+                            if (audioCount == 0 || audioText.length > 50) {
+                                onAudioTextReady?.invoke(
+                                    audioText,
+                                    promptBag.promptId
+                                )  // Call only if not null
+                                startIndex = firstNewLineIndex + 1
+                                tempIndex = startIndex
+                                audioCount++
+                                if (audioCount > 1 && !summarySent) {
+                                    onSummaryReady?.invoke(
+                                        fullText,
+                                        promptBag.promptId
+                                    )  // Call only if not null
+                                    summarySent = true
+                                }
+                            } else {
+                                //force to move to the next paragraph
+                                tempIndex = firstNewLineIndex + 1
+                            }
+                            Log.d("TextService", "Sending one paragraph of audio")
+                        }
+                        //responseState.postValue(fullText)
+                        onTextUpdated(fullText, promptBag.promptId)
+                    }
+                    .onCompletion {
+                        onAudioTextReady?.invoke(
+                            fullText.substring(startIndex),
+                            promptBag.promptId
+                        )  // Call only if not null
+                        //responseState.postValue(fullText)
+                        onTextUpdated(fullText, promptBag.promptId)
+                        resetText()
+                        onCompleted?.invoke(promptBag.promptId)
+                        onCompletedSuspend?.invoke(promptBag.promptId)
+                    }
+                    .launchIn(coroutineScope)
+            }catch (e: Exception){
+                onError?.invoke(e.message.toString())
+            }
+
         }
     }
 
@@ -98,27 +116,27 @@ class TextService(private val openAI: OpenAI) {
         audioCount = 0
         summarySent = false
     }
-
-    fun get(coroutineScope: CoroutineScope, prompt: String) {
-        coroutineScope.launch {
-            println("\n> Create chat completions...")
-            val chatCompletionRequest = ChatCompletionRequest(
-                model = ModelId("gpt-3.5-turbo"),
-                messages = listOf(
-                    ChatMessage(
-                        role = ChatRole.System,
-                        content = "You are a very helpful assistant."),
-                    ChatMessage(role = ChatRole.User, content = prompt)
-                )
-            )
-            val completion = openAI.chatCompletion(chatCompletionRequest)
-            completion.choices.forEach(::println)
-
-            println("\n>️ Creating chat completions stream...")
-            openAI.chatCompletions(chatCompletionRequest)
-                .onEach { print(it.choices.firstOrNull()?.delta?.content.orEmpty()) }
-                .onCompletion { println("Stream completed.") }
-                .launchIn(coroutineScope)
-        }
-    }
+//
+//    fun get(coroutineScope: CoroutineScope, prompt: String) {
+//        coroutineScope.launch {
+//            println("\n> Create chat completions...")
+//            val chatCompletionRequest = ChatCompletionRequest(
+//                model = ModelId("gpt-3.5-turbo"),
+//                messages = listOf(
+//                    ChatMessage(
+//                        role = ChatRole.System,
+//                        content = "You are a very helpful assistant."),
+//                    ChatMessage(role = ChatRole.User, content = prompt)
+//                )
+//            )
+//            val completion = openAI.chatCompletion(chatCompletionRequest)
+//            completion.choices.forEach(::println)
+//
+//            println("\n>️ Creating chat completions stream...")
+//            openAI.chatCompletions(chatCompletionRequest)
+//                .onEach { print(it.choices.firstOrNull()?.delta?.content.orEmpty()) }
+//                .onCompletion { println("Stream completed.") }
+//                .launchIn(coroutineScope)
+//        }
+//    }
 }

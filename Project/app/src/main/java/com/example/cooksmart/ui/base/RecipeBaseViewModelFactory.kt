26c862e6d5234a4228be93b2b4cec8d6195eea90
Awesomedonut <Jessicayjsong@gmail.com>
2023-12-01 15:18:59 -0800

    Migrate some functions from RecipeFragment/RecipeViewModel to RecipeBase classes

--- Project/app/src/main/java/com/example/cooksmart/ui/base/RecipeBaseViewModelFactory.kt
+++ Project/app/src/main/java/com/example/cooksmart/ui/base/RecipeBaseViewModelFactory.kt
@@ -1,4 +1,21 @@
 package com.example.cooksmart.ui.base
 
-class RecipeBaseViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.cooksmart.utils.DataFetcher
+
+class RecipeBaseViewModelFactory(
+    private val fetcher: DataFetcher,
+    private val application: Application,
+    private val childViewModelClass: Class<out RecipeBaseViewModel>
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(childViewModelClass)) {
+            @Suppress("UNCHECKED_CAST")
+            return childViewModelClass.getConstructor(DataFetcher::class.java, Application::class.java)
+                .newInstance(fetcher, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }

7fdea8f71efef1b59d5092a5658cb28047af002c
Amanda <88062177+amandahn@users.noreply.github.com>
2023-12-02 14:29:58 -0800

    - Added permission check util

---
+++ Project/app/src/main/java/com/example/cooksmart/utils/PermissionCheck.kt
@@ -0,0 +1,30 @@
+package com.example.cooksmart.utils
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+private const val reqCode : Int = 0
+object PermissionCheck {
+    fun checkPermissions(activity: Activity?) {
+        if (Build.VERSION.SDK_INT < 23) return
+        if (ContextCompat.checkSelfPermission(activity!!, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(activity!!, Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(activity!!, Manifest.permission.READ_MEDIA_VIDEO) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(activity!!, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(activity!!, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(activity,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_MEDIA_AUDIO,
+                    Manifest.permission.READ_MEDIA_VIDEO,
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.POST_NOTIFICATIONS,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION), reqCode)
+        }
+    }
+}

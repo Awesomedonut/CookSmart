758c4ebaf386172696d1c131a2742620a8abe1cd
Awesomedonut <Jessicayjsong@gmail.com>
2023-12-02 15:35:43 -0800

    clean up

--- Project/app/src/main/java/com/example/cooksmart/Constants.kt
+++ Project/app/src/main/java/com/example/cooksmart/Constants.kt
@@ -1,12 +1,6 @@
 package com.example.cooksmart
 
-import android.content.Intent
-import android.media.MediaPlayer
-import android.net.Uri
-import androidx.activity.result.ActivityResultLauncher
-
 object Constants {
-    const val REQUEST_CODE_SPEECH_INPUT = 1
     const val CAMERA_PERMISSION_REQUEST_CODE = 101
     const val PACKAGE_NAME = "com.example.cooksmart"
     const val INGRE_IMG_FILE_NAME = "ingre_img.jpg"

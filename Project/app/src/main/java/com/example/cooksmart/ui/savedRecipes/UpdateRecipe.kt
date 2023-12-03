a6d2e97ef0781efae145f006518cd48ef044acd2
Sarah <scli@sfu.ca>
2023-12-03 01:26:53 -0800

    Make add/update recipe able to scroll. General formatting with margins

--- Project/app/src/main/java/com/example/cooksmart/ui/savedRecipes/UpdateRecipe.kt
+++ Project/app/src/main/java/com/example/cooksmart/ui/savedRecipes/UpdateRecipe.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider

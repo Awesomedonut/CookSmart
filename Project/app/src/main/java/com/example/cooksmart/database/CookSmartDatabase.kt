9b2a2c361b3864585f0199b75dc73e3fa29390a4
Sarah <scli@sfu.ca>
2023-12-05 20:26:43 -0800

    increase room version

--- Project/app/src/main/java/com/example/cooksmart/database/CookSmartDatabase.kt
+++ Project/app/src/main/java/com/example/cooksmart/database/CookSmartDatabase.kt
@@ -12,7 +12,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Ingredient::class, Recipe::class, Calendar::class], version = 2)
+@Database(entities = [Ingredient::class, Recipe::class, Calendar::class], version = 3)
 abstract class CookSmartDatabase : RoomDatabase() {
     abstract fun ingredientDao(): IngredientDao
     abstract fun recipeDao(): RecipeDao
